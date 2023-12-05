25cd1620ca0122db84e17fdccdd0be0afba11a2b
hyim <gjs9293@gmail.com>
2023-12-05 22:29:44 +0900

    5_1

--- Practice/5_1/reader.py
+++ Practice/5_1/reader.py
@@ -1,6 +1,7 @@
 # reader.py
 
 import csv
+from  typing import List
 
 def read_csv_as_dicts(filename, types):
     '''
@@ -8,12 +9,7 @@ def read_csv_as_dicts(filename, types):
     '''
     records = []
     with open(filename) as file:
-        rows = csv.reader(file)
-        headers = next(rows)
-        for row in rows:
-            record = { name: func(val)
-                      for name, func, val in zip(headers, types, row) }
-            records.append(record)
+        records = csv_as_dicts(file, types)
     return records
 
 def read_csv_as_instances(filename, cls):
@@ -22,26 +18,26 @@ def read_csv_as_instances(filename, cls):
     '''
     records = []
     with open(filename) as file:
-        rows = csv.reader(file)
-        headers = next(rows)
-        for row in rows:
-            record = cls.from_row(row)
-            records.append(record)
+        records = csv_as_instances(file, cls)
     return records
 
-def csv_as_dicts(lines, types):
+def csv_as_dicts(lines: List[str], types: List[function], headers: List[str]=None) -> List:
     records = []
-    headers = next(lines)
-    for line in lines:
+    rows = csv.reader(lines)
+    if headers is None:
+        headers = next(rows)
+    for row in rows:
         record = { name: func(val)
-                  for name, func, val in zip(headers, types, line.split()) }
+                  for name, func, val in zip(headers, types, row) }
         records.append(record)
     return records
 
-def csv_as_instances(lines, cls):
+def csv_as_instances(lines: List[bytes], cls, headers: List[str]=None) -> List:
     records = []
-    headers = next(lines)
-    for line in lines:
-        record = cls.from_row(line)
+    rows = csv.reader(lines)
+    if headers is None:
+        headers = next(rows)
+    for row in rows:
+        record = cls.from_row(row)
         records.append(record)
     return records
