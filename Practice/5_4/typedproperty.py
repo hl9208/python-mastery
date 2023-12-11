5f6af5316520c21c1c93493b281f7bb50416a8b9
hyim <gjs9293@gmail.com>
2023-12-11 23:01:54 +0900

    5_4 미해결, 메소드 안에 클래스 선언해서 해결해보는 중

--- Practice/5_4/typedproperty.py
+++ Practice/5_4/typedproperty.py
@@ -1,16 +1,26 @@
 # typedproperty.py
 
-def typedproperty(name, expected_type):
-    private_name = '_' + name
+def typedproperty(expected_type):
+    class TypedProperty:
+        def __init__(self, expected_type):
+            self.expected_type
+            self.private_name = None
 
-    @property
-    def value(self):
-        return getattr(self, private_name)
+        def __set_name__(self, cls, name):
+            self.private_name = name
 
-    @value.setter
-    def value(self, val):
-        if not isinstance(val, expected_type):
-            raise TypeError(f'Expected {expected_type}')
-        setattr(self, private_name, val)
+        @property
+        def value(self):
+            return getattr(self, self.private_name)
 
-    return value
+        @value.setter
+        def value(self, val):
+            if not isinstance(val, self.expected_type):
+                raise TypeError(f'Expected {self.expected_type}')
+            setattr(self, self.private_name, val)
+
+    return TypedProperty
+
+String = lambda : typedproperty(str)
+Integer = lambda : typedproperty(int)
+Float = lambda : typedproperty(float)
